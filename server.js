c6446829d9c3d9f8f0d9dee8b08e02e4b55ad992
DIKSINTH <diksinth1@gmail.com>
2025-11-20 16:39:28 +0530

    new

--- server.js
+++ server.js
@@ -22,11 +22,13 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/admin", blogRoute);
 
-// MongoDB Connection
 mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("MongoDB Connected Successfully"))
-  .catch((err) => console.log("MongoDB Error:", err.message));
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server Running on Port ${PORT}`));
